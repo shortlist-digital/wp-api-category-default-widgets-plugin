bbd3630563b449993fdb8fca930f1bdf8f80a87c
ecoad <elliot.coad@shortlist.com>
2017-07-17 14:39:22 +0100

    init

---
+++ readme.md
@@ -0,0 +1,2 @@
+# WP API Category Default Widgets Plugin
+Create default widgets for Categories that have no widgets set
---
+++ wp-api-category-default-widgets-plugin.php
@@ -0,0 +1,22 @@
+<?php
+/**
+* @wordpress-plugin
+* Plugin Name: WP API Category Default Widgets Plugin
+* Plugin URI: http://github.com/shortlist-digital/wp-api-category-default-widgets-plugin
+* Description: Create default widgets for Categories that have no widgets set
+* Version: 1.0.0
+* Author: Shortlist Studio
+* Author URI: http://shortlist.studio
+* License: MIT
+*/
+
+require_once __DIR__ . '/../../../../vendor/autoload.php';
+
+class WpApiCategoryDefaultWidgetsPlugin
+{
+    public function __construct()
+    {
+        //
+    }
+}
+new WpApiCategoryDefaultWidgetsPlugin();
